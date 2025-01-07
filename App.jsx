f8a0e15e2b453165db9155e3a5b7c7d42b90661b
Online Developer <onlinedeveloper4u@gmail.com>
2025-01-07 19:34:33 +0500

    View, Button, Text

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={ { fontSize : 30 } }>App</Text>
+      <Button title='Press Here' />
+    </View>
+  )
+}
+
+export default App
