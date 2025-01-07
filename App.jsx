41c9b9d861c17f25554621eeb87b94761067fec0
Online Developer <onlinedeveloper4u@gmail.com>
2025-01-07 20:04:03 +0500

    Component

--- App.jsx
+++ App.jsx
@@ -1,11 +1,17 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
+import First from './src/components/First'
 
 const App = () => {
   return (
     <View>
-      <Text style={ { fontSize : 30 } }>App</Text>
+      <Text style={ { fontSize : 30 } }>React Native Mobile APP Developer</Text>
+      <Text style={ { fontSize : 30 } }>Muhammad Aqib</Text>
       <Button title='Press Here' />
+      <First />
+      <First />
+      <First />
+      <First />
     </View>
   )
 }
---
+++ src/components/First.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const First = () => {
+    return(
+        <View>
+            <Text style={ { fontSize : 30 } }>React Native Tutorial</Text>
+        </View>
+    )
+}
+
+export default First
