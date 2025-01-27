85e228824a420d164e844fe498fa127ea3fca0fa
Muhammad Aqib <onlinedeveloper4u@gmail.com>
2025-01-27 23:42:38 +0500

    Platform Specific Code

--- App.jsx
+++ App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 // import { View, Text, Button } from 'react-native'
 import { View } from 'react-native'
-import GET_API_LIST from './src/components/GET_API_LIST'
+import PlatformExample from './src/components/PlatformExample'
+// import GET_API_LIST from './src/components/GET_API_LIST'
 // import DELETE_API from './src/components/DELETE_API'
 // import PATCH_API from './src/components/PATCH_API'
 // import PUT_API from './src/components/PUT_API'
@@ -70,7 +71,8 @@ const App = () => {
       {/* <PUT_API /> */}
       {/* <PATCH_API /> */}
       {/* <DELETE_API /> */}
-      <GET_API_LIST />
+      {/* <GET_API_LIST /> */}
+      <PlatformExample />
     </View>
   )
 }
