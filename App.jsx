37dcedfadd29eb1a285132724d27a17b1da33c1a
Muhammad Aqib <onlinedeveloper4u@gmail.com>
2025-01-30 23:27:31 +0500

    Reanimated Installation, Animated Flip Card UI

--- App.jsx
+++ App.jsx
@@ -1,14 +1,13 @@
 import './global.css'
 import React from 'react'
-import { SafeAreaView, View } from 'react-native'
-import One from './src/components/One'
-import MyLogin from './src/components/MyLogin'
+import { View } from 'react-native'
+import AnimatedCardFlip from './src/components/AnimatedCardFlip'
 
 const App = () => {
   return (
-    <SafeAreaView className="flex-1">
-      <MyLogin />
-    </SafeAreaView>
+    <View style={{flex: 1}}>
+      <AnimatedCardFlip />
+    </View>
   )
 }
 
