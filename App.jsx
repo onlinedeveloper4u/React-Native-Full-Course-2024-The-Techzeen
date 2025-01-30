4cb0f2669562671bf2a4990291f5b1b176c1a03b
Muhammad Aqib <onlinedeveloper4u@gmail.com>
2025-01-30 15:26:53 +0500

    Imported gesture handler package

--- App.jsx
+++ App.jsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler'
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
