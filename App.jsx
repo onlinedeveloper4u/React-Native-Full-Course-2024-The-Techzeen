c4a5ce84a8038741ca19d73a544e066957ee9bdb
Muhammad Aqib <onlinedeveloper4u@gmail.com>
2025-02-02 19:21:40 +0500

    Firebase Firestore CRUD

--- App.jsx
+++ App.jsx
@@ -1,23 +1,14 @@
 // Firebase Authentication
 import './global.css'
 import React from 'react'
-import Login from './src/components/Login'
-import Register from './src/components/Register'
-import ForgotPassword from './src/components/ForgotPassword'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
-const Stack = createNativeStackNavigator()
+import UserCRUD from './src/components/UserCRUD'
+import { View } from 'react-native'
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-        <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{headerShown: false}} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View className="flex-1">
+      <UserCRUD />
+    </View>
   )
 }
 
